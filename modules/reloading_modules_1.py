9dcb7ac1c790c35d37f6bdc6f9e8dfd85d1332a2
Daniel Tomaszuk <dtomaszuk01@gmail.com>
2024-09-08 15:17:03 +0200

    Add module reload examples

---
+++ modules/reloading_modules_1.py
@@ -0,0 +1,8 @@
+# reloading_modules_1.py
+
+print('running reloading_modules_1.py...')
+
+def print_values():
+	print('k1', '10')
+	print('k2', '20')
+	print('k3', 'abcd')
---
+++ modules/reloading_modules_2.py
@@ -0,0 +1,7 @@
+# reloading_modules_2.py
+
+print('running reloading_modules_2.py...')
+
+def print_values():
+	print('module_num', '2')
+	print('more_info', 'a')
